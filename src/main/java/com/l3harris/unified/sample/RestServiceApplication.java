431f1184de53d4bb2517b00822c3afb910b3c409
jdellostritto <jim.dellostritto@gmail.com>
2020-10-15 09:07:28 -0400

    updates for section 4

--- src/main/java/com/l3harris/unified/sample/RestServiceApplication.java
+++ src/main/java/com/l3harris/unified/sample/RestServiceApplication.java
@@ -1,13 +1,24 @@
 package com.l3harris.unified.sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestServiceApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestServiceApplication.class);
+
     public static void main(String[] args) {
+        
         SpringApplication.run(RestServiceApplication.class, args);
+        LOGGER.info("Application Started: {} ", LocalDateTime.now());
+        LOGGER.debug("Application Started: {} ", LocalDateTime.now());
+        LOGGER.error("Application Started: {} ", LocalDateTime.now());
     }
 
 }
