f3cd204714d9031d419c9adb1b8042927229a711
jdellostritto <jim.dellostritto@gmail.com>
2020-12-17 20:20:14 -0500

    moved db connection string and user to envars.*

--- kubernetes/envars.sh
+++ kubernetes/envars.sh
@@ -4,3 +4,6 @@ export ENVIRONMENT=default
 export BUILD=latest
 export REPCOUNT=1
 export PROJECT=template-service-java
+export DB_CONNECTION_STRING=jdbc:postgresql://postgres:5432/sample
+export DB_USER=sampleuser
+export DB_PWD=samplepass
